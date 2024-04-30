9e366ef6a32a5db1aaf24d9f5605286670eb0323
Isaiah-o <167384925+Isaiah-o@users.noreply.github.com>
2024-04-29 21:31:41 -0700

    Add files via upload

---
+++ aaa.js
@@ -0,0 +1,30 @@
+const ac = 'You have received this message because you have because you have been chosen to open an important vault'
+const ab =  'Here is the secret combination'
+
+const combinedstring = ac + ab 
+console.log(combinedstring)
+
+//added first number
+
+let n1 = 5
+let n2 = 2
+let combs2 = n1 * n2 
+console.log(combs2)
+
+//added second number
+
+let n3 = 50
+let n4 = 10
+let combs3 = n3 - n4 
+console.log(combs3)
+
+//added second number
+
+let n5 = 13
+let n6 = 3
+let combs4 = n5 * n6 
+console.log(combs4)
+
+const answers = '10, 39, 40'
+
+alert(answers)
